a053c0f760eae713e2e5fdf33c8f132065d9146b
pH00 <pascalhuwart@yahoo.com>
2019-12-07 22:29:43 -0600

    MyWidget

---
+++ mayas_guatemala_app02/widgets/samplewidgets/MyWidget/Widget.js
@@ -0,0 +1,11 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://js.arcgis.com/3.15/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+//>>built
+define(['dojo/_base/declare', 'jimu/BaseWidget'],
+function(declare, BaseWidget) {
+  //To create a widget, you need to derive from BaseWidget.
+  return declare([BaseWidget], {
+    // Custom widget code goes here 
+    baseClass: 'jimu-widget-mywidget'
+  });
+});
