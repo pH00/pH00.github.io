80b547ad19d5f5ec03fffcb36923fc6a5958e9f9
pH00 <pascalhuwart@yahoo.com>
2019-12-08 18:49:38 -0600

    try this

---
+++ mayas_guatemala_app02/widgets/MyWidget/Widget.js
@@ -0,0 +1,14 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://js.arcgis.com/3.15/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+//>>built
+define(['dojo/_base/declare', 'jimu/BaseWidget'],
+function(declare, BaseWidget) {
+  //To create a widget, you need to derive from BaseWidget.
+  return declare([BaseWidget], {
+	startup: function() {
+	      this.inherited(arguments);
+	      this.mapIdNode.innerHTML = 'map id is:' + this.map.id;
+	    },
+    baseClass: 'jimu-widget-mywidget'
+  });
+});
